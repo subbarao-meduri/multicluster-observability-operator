62b029eee56414f3c916131accd7120657debf37
Subbarao Meduri <smeduri@redhat.com>
2022-08-04 09:36:02 -0400

    more logging
    Signed-off-by: Subbarao Meduri <smeduri@redhat.com>

--- proxy/pkg/proxy/proxy.go
+++ proxy/pkg/proxy/proxy.go
@@ -7,6 +7,7 @@ import (
 	"bytes"
 	"compress/gzip"
 	"errors"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -33,8 +34,38 @@ var (
 	serverHost   = ""
 )
 
+// formatRequest generates ascii representation of a request
+func formatRequest(r *http.Request) string {
+	// Create return string
+	var request []string
+	// Add the request string
+	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
+	request = append(request, url)
+	// Add the host
+	request = append(request, fmt.Sprintf("Host: %v", r.Host))
+	// Loop through headers
+	for name, headers := range r.Header {
+		name = strings.ToLower(name)
+		for _, h := range headers {
+			request = append(request, fmt.Sprintf("%v: %v", name, h))
+		}
+	}
+
+	// If this is a POST, add post data
+	if r.Method == "POST" {
+		r.ParseForm()
+		request = append(request, "\n")
+		request = append(request, r.Form.Encode())
+	}
+	// Return the request as a string
+	return strings.Join(request, "\n")
+}
+
 // HandleRequestAndRedirect is used to init proxy handler
 func HandleRequestAndRedirect(res http.ResponseWriter, req *http.Request) {
+	klog.Infof("--------- Request Headers begin -------")
+	klog.Infof(formatRequest(req))
+	klog.Infof("--------- Request Headers end ---------")
 	if preCheckRequest(req) != nil {
 		klog.Infof("Writing a newEmptyMatrixHTTPBody")
 		_, err := res.Write(newEmptyMatrixHTTPBody())
